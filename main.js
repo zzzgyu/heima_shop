30747579587a7d9363d4ac054bbc57e7208e192f
zzzgy <1589953744@qq.com>
2021-07-23 15:37:08 +0800

    完成了首页的开发

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 import Vue from 'vue'
 import App from './App'
+//按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
 
+
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  // do somethimg...
+  uni.showLoading({
+  	title:'加载数据中...'
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  // do something...
+  uni.hideLoading()
+}
 Vue.config.productionTip = false
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 
 App.mpType = 'app'
 
